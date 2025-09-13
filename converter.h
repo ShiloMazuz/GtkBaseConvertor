1e5ac0e3289833b1dd199ca13a365697bec21058
Shilo <shilo@localhost.localdomain>
2025-09-13 16:38:00 +0300

    added definitions for converter functions in order to use in main

---
+++ converter.h
@@ -0,0 +1,15 @@
+#ifndef CONVERTER_H
+#define CONVERTER_H
+
+#include <iostream>
+#include <vector>
+#include <string>
+
+std::vector<bool> decimalToBinary (int num);
+std::vector<int> decimalToHex(int num);
+void printBinaryNumber(std::vector<bool>& binaryForm);
+void printHexNumber(std::vector<int>& hexForm);
+std::string hexVectorToString(std::vector<int> hexForm);
+std::string decimalToHexString(int num);
+
+#endif
