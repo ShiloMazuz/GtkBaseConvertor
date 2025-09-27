c0cd8053b311ee541b2fb42ef6821e5851176b92
Shilo <shiloteam@gmail.com>
2025-09-27 18:46:42 +0300

    changed widget calls into pointers to be used via gtk builder, removed hex number converter section

--- build/main.cpp
+++ build/main.cpp
@@ -5,26 +5,35 @@
 
 class MyWindow : public Gtk::Window {
 public:
-    MyWindow()
-        : adjustment(Gtk::Adjustment::create(0.0, 0.0, 10e9, 1.0, 10.0, 0.0)),
-        entry(adjustment, 1.0, 0) // climb rate = 1.0, digits = 0
+    MyWindow(BaseObjectType* cobject,
+             const Glib::RefPtr<Gtk::Builder>& builder)
+        : adjustment(Gtk::Adjustment::create(0.0, 0.0, 10e9, 1.0, 10.0, 0.0))
+        , Gtk::Window(cobject)
+        //entry(adjustment, 1.0, 0) // climb rate = 1.0, digits = 0
     {
         set_title("Ip Converter");
         set_default_size(300, 100);
 
-        vbox.set_spacing(10);
-        add(vbox);
-        vbox.pack_start(entry, Gtk::PACK_SHRINK);
-        //Gtk::PACK_SHRINK is equivilant to ...false, false, 0);
+  //      grid.set_row_spacing(10);
+  //      add(grid);
+        builder->get_widget("grid", grid);
+        builder->get_widget("entryIpInput", entryIpInput);
+        builder->get_widget("entryPrefix", entryPrefix);
+        builder->get_widget("labelIpBin", labelIpBin);
+        builder->get_widget("labelSubnetMask", labelSubnetMask);
+        builder->get_widget("buttonIpConvert", buttonIpConvert);
 
-        label.set_text("WAWI!!! (hex)");
-        vbox.pack_start(label, Gtk::PACK_SHRINK);
+  //     grid.attach(entry, Gtk::PACK_SHRINK);
+   //     //Gtk::PACK_SHRINK is equivilant to ...false, false, 0);
 
-        labelBin.set_text("WAWI!!! (bin)");
-        vbox.pack_start(labelBin, Gtk::PACK_SHRINK);
+   //     label.set_text("WAWI!!! (hex)");
+   //     grid.attach(label, Gtk::PACK_SHRINK);
 
-        button.set_label("press this pls");
-        vbox.pack_start(button, false, false, 0); 
+   //     labelBin.set_text("WAWI!!! (bin)");
+   //     grid.attach(labelBin, Gtk::PACK_SHRINK);
+
+   //     button.set_label("press this pls");
+   //     grid.attach(button, false, false, 0); 
         //the first false, disables centering the widget in the space it has available,
         //the second false, disables expanding the widget to the available space
 
@@ -33,56 +42,59 @@ public:
 //                label.set_text(entry.get_text());
 //        }); //I need to figure out what the fuck happens here
 
-        button.signal_clicked().connect(
-            [&] {
-                label.set_text("Hex Value: " + decimalToHexString(entry.get_value_as_int()));
-                labelBin.set_text("Bin Value: " + decimalToBinString(entry.get_value_as_int()));
-                std::cout << decimalToHexString(entry.get_value_as_int()) << '\n';
-                               }); //I need to figure out what the fuck happens here
+//        button.signal_clicked().connect(
+//            [&] {
+//                label.set_text("Hex Value: " + decimalToHexString(entry.get_value_as_int()));
+//                labelBin.set_text("Bin Value: " + decimalToBinString(entry.get_value_as_int()));
+//                std::cout << decimalToHexString(entry.get_value_as_int()) << '\n';
+//                               }); //I need to figure out what the fuck happens here
         //ip converter section
-        entryIpInput.set_placeholder_text("enter an ip address");
-        vbox.pack_start(entryIpInput, Gtk::PACK_SHRINK);
+//        entryIpInput.set_placeholder_text("enter an ip address");
+//        grid.attach(entryIpInput, 0, 0, 1, 1);
+//        //col, row, col size, row size
+//        //label to show converted ip adress
+//        labelIpBin.set_text("Bin ip:");
+//        grid.attach(labelIpBin, 0, 1, 1, 1);
+//
+//        entryPrefix.set_placeholder_text("prefix");
+//        grid.attach(entryPrefix, 1, 0, 1, 1);
+//
+//        labelSubnetMask.set_text("Subnet Mask:");
+//        grid.attach(labelSubnetMask, 0, 2, 1, 1);
+//
+//        //button that converts ip address and prints labels
+//        buttonIpConvert.set_label("press to convert ip to bin");
+//        grid.attach(buttonIpConvert, 0, 3, 1, 1); 
 
-        //label to show converted ip adress
-        labelIpBin.set_text("Bin ip:");
-        vbox.pack_start(labelIpBin, Gtk::PACK_SHRINK);
-
-        entryPrefix.set_placeholder_text("prefix");
-        vbox.pack_start(entryPrefix, Gtk::PACK_SHRINK);
-
-        labelSubnetMask.set_text("Subnet Mask:");
-        vbox.pack_start(labelSubnetMask, Gtk::PACK_SHRINK);
-
-        //button that converts ip address and prints labels
-        buttonIpConvert.set_label("press to convert ip to bin");
-        vbox.pack_start(buttonIpConvert, false, false, 0); 
-
-        buttonIpConvert.signal_clicked().connect(
+        buttonIpConvert->signal_clicked().connect(
             [&] {
-                labelIpBin.set_text("Bin ip: " + convertedIpToBin(extractIpFromString(entryIpInput.get_text())));
-                labelSubnetMask.set_text("Subnet Mask: " + prefixToSubnetString(entryPrefix.get_text()));
+                labelIpBin->set_text("Bin ip: " + convertedIpToBin(extractIpFromString(entryIpInput->get_text())));
+                labelSubnetMask->set_text("Subnet Mask: " + prefixToSubnetString(entryPrefix->get_text()));
                                }); //I need to figure out what the fuck happens here
 
         show_all_children();
     }
 private:
-    Gtk::Box vbox{Gtk::ORIENTATION_VERTICAL};
+    Gtk::Grid* grid;
     Glib::RefPtr<Gtk::Adjustment> adjustment;
     //number conversion widgets
-    Gtk::SpinButton entry;
-    Gtk::Label label;
-    Gtk::Label labelBin;
-    Gtk::Button button;
+//    Gtk::SpinButton entry;
+//    Gtk::Label label;
+//    Gtk::Label labelBin;
+//    Gtk::Button button;
     //ip conversion widgets
-    Gtk::Entry entryIpInput;
-    Gtk::Label labelIpBin;
-    Gtk::Entry entryPrefix;
-    Gtk::Label labelSubnetMask;
-    Gtk::Button buttonIpConvert;
+    Gtk::Entry* entryIpInput;
+    Gtk::Entry* entryPrefix;
+    Gtk::Label* labelIpBin;
+    Gtk::Label* labelSubnetMask;
+    Gtk::Button* buttonIpConvert;
 };
 
 int main(int argc, char *argv[]) {
     auto app = Gtk::Application::create(argc, argv, "org.gnome.typeconverter");
-    MyWindow window {};
-    return app->run(window);
+    //auto builder = Gtk::Builder::create_from_file("../styling/layout.ui");
+    auto builder = Gtk::Builder::create_from_file("../styling/layout.ui");
+    MyWindow* window = nullptr;
+    builder->get_widget_derived("window", window);
+    return app->run(*window);
 }
