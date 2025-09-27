bf1230684810f3e510f79bf4f5ff753c8b47fb64
Shilo <shiloteam@gmail.com>
2025-09-27 18:48:05 +0300

    removed unused code (used while the UI was statically typed)

--- build/main.cpp
+++ build/main.cpp
@@ -9,62 +9,16 @@ public:
              const Glib::RefPtr<Gtk::Builder>& builder)
         : adjustment(Gtk::Adjustment::create(0.0, 0.0, 10e9, 1.0, 10.0, 0.0))
         , Gtk::Window(cobject)
-        //entry(adjustment, 1.0, 0) // climb rate = 1.0, digits = 0
     {
         set_title("Ip Converter");
         set_default_size(300, 100);
 
-  //      grid.set_row_spacing(10);
-  //      add(grid);
         builder->get_widget("grid", grid);
         builder->get_widget("entryIpInput", entryIpInput);
         builder->get_widget("entryPrefix", entryPrefix);
         builder->get_widget("labelIpBin", labelIpBin);
         builder->get_widget("labelSubnetMask", labelSubnetMask);
         builder->get_widget("buttonIpConvert", buttonIpConvert);
-
-  //     grid.attach(entry, Gtk::PACK_SHRINK);
-   //     //Gtk::PACK_SHRINK is equivilant to ...false, false, 0);
-
-   //     label.set_text("WAWI!!! (hex)");
-   //     grid.attach(label, Gtk::PACK_SHRINK);
-
-   //     labelBin.set_text("WAWI!!! (bin)");
-   //     grid.attach(labelBin, Gtk::PACK_SHRINK);
-
-   //     button.set_label("press this pls");
-   //     grid.attach(button, false, false, 0); 
-        //the first false, disables centering the widget in the space it has available,
-        //the second false, disables expanding the widget to the available space
-
-//        button.signal_clicked().connect(
-//            [&] {
-//                label.set_text(entry.get_text());
-//        }); //I need to figure out what the fuck happens here
-
-//        button.signal_clicked().connect(
-//            [&] {
-//                label.set_text("Hex Value: " + decimalToHexString(entry.get_value_as_int()));
-//                labelBin.set_text("Bin Value: " + decimalToBinString(entry.get_value_as_int()));
-//                std::cout << decimalToHexString(entry.get_value_as_int()) << '\n';
-//                               }); //I need to figure out what the fuck happens here
-        //ip converter section
-//        entryIpInput.set_placeholder_text("enter an ip address");
-//        grid.attach(entryIpInput, 0, 0, 1, 1);
-//        //col, row, col size, row size
-//        //label to show converted ip adress
-//        labelIpBin.set_text("Bin ip:");
-//        grid.attach(labelIpBin, 0, 1, 1, 1);
-//
-//        entryPrefix.set_placeholder_text("prefix");
-//        grid.attach(entryPrefix, 1, 0, 1, 1);
-//
-//        labelSubnetMask.set_text("Subnet Mask:");
-//        grid.attach(labelSubnetMask, 0, 2, 1, 1);
-//
-//        //button that converts ip address and prints labels
-//        buttonIpConvert.set_label("press to convert ip to bin");
-//        grid.attach(buttonIpConvert, 0, 3, 1, 1); 
 
         buttonIpConvert->signal_clicked().connect(
             [&] {
@@ -77,12 +31,6 @@ public:
 private:
     Gtk::Grid* grid;
     Glib::RefPtr<Gtk::Adjustment> adjustment;
-    //number conversion widgets
-//    Gtk::SpinButton entry;
-//    Gtk::Label label;
-//    Gtk::Label labelBin;
-//    Gtk::Button button;
-    //ip conversion widgets
     Gtk::Entry* entryIpInput;
     Gtk::Entry* entryPrefix;
     Gtk::Label* labelIpBin;
